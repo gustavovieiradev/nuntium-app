e64a1aefdc7496c294eeb44462efd36f6d24c2e0
Gustavo Vieira <gustavovieiradev@gmail.com>
2021-03-17 18:41:41 -0300

    feat(bookmarks): create list news

--- src/pages/Bookmarks/index.tsx
+++ src/pages/Bookmarks/index.tsx
@@ -1,11 +1,40 @@
 import React from 'react';
+import { ScrollView } from 'react-native-gesture-handler';
 import { Header } from '../../components/Header';
-import { Container } from './styles';
+
+import { Container, News, NewsItem, NewsImage, NewsData, NewsTag, NewsTitle } from './styles';
+
+import imgList from '../../images/imgList1.png';
 
 export default function Bookmarks() {
   return (
     <Container>
       <Header title="Bookmarks" description="Saved articles to the library" />
+      <ScrollView style={{flex: 1}} showsVerticalScrollIndicator={false}>
+        <News>
+          <NewsItem>
+            <NewsImage source={imgList} />
+            <NewsData>
+              <NewsTag>UI/UX Design</NewsTag>
+              <NewsTitle>A Simple Trick For Creating Color Palettes Quickly</NewsTitle>
+            </NewsData>
+          </NewsItem>
+          <NewsItem>
+            <NewsImage source={imgList} />
+            <NewsData>
+              <NewsTag>UI/UX Design</NewsTag>
+              <NewsTitle>A Simple Trick For Creating Color Palettes Quickly</NewsTitle>
+            </NewsData>
+          </NewsItem>
+          <NewsItem>
+            <NewsImage source={imgList} />
+            <NewsData>
+              <NewsTag>UI/UX Design</NewsTag>
+              <NewsTitle>A Simple Trick For Creating Color Palettes Quickly</NewsTitle>
+            </NewsData>
+          </NewsItem>
+        </News>
+      </ScrollView>
     </Container>
   )
 };
--- src/pages/Bookmarks/styles.ts
+++ src/pages/Bookmarks/styles.ts
@@ -5,3 +5,37 @@ export const Container = styled.View`
   background-color: #fff;
   padding: 70px 20px 20px 20px;
 `;
+
+export const News = styled.View`
+  margin-top: 25px;
+`;
+
+export const NewsItem = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  height: 96px;
+  margin-bottom: 16px;
+`;
+
+export const NewsImage = styled.Image``;
+
+export const NewsTag = styled.Text`
+  font-size: 14px;
+  color: #7C82A1;
+  font-family: 'OpenSans_400Regular';
+`;
+
+export const NewsTitle = styled.Text`
+  color: #333647;
+  font-size: 16px;
+  font-family: 'OpenSans_600SemiBold';
+  margin-top: 8px;
+`;
+
+export const NewsData = styled.View`
+  flex: 1;
+  flex-direction: column;
+  margin-left: 15px;
+`;

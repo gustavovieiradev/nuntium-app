dc153f4c930cc5f023a7f03c813efae838ae63dd
Gustavo Vieira <gustavovieiradev@gmail.com>
2021-03-17 19:36:18 -0300

    feat(profile): create page profile

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,11 +1,55 @@
 import React from 'react';
+import { Feather } from '@expo/vector-icons';
+
 import { Header } from '../../components/Header';
-import { Container } from './styles';
+import { Container, Email, ImgUser, Info, InfoData, Name, Menu, ItemMenu, ItemName } from './styles';
+
+import userImg from '../../images/user.png';
+import { Switch } from 'react-native';
 
 export default function Profile() {
   return (
     <Container>
       <Header title="Profile" />
+
+      <Info>
+        <ImgUser source={userImg} />
+        <InfoData>
+          <Name>Eren Turkmen</Name>
+          <Email>ertuken@gmail.com</Email>
+        </InfoData>
+      </Info>
+
+      <Menu>
+        <ItemMenu>
+          <ItemName>Notifications</ItemName>
+          <Switch 
+            thumbColor="#fff" 
+            trackColor={{ false: '#ccc', true: '#475AD7' }}
+          />
+        </ItemMenu>
+        <ItemMenu>
+          <ItemName>Language</ItemName>
+          <Feather name="chevron-right" color="#666C8E" size={20} />
+        </ItemMenu>
+        <ItemMenu>
+          <ItemName>Change Password</ItemName>
+          <Feather name="chevron-right" color="#666C8E" size={20} />
+        </ItemMenu>
+        <ItemMenu>
+          <ItemName>Privacy</ItemName>
+          <Feather name="chevron-right" color="#666C8E" size={20} />
+        </ItemMenu>
+        <ItemMenu>
+          <ItemName>Terms {"&"} Conditions</ItemName>
+          <Feather name="chevron-right" color="#666C8E" size={20} />
+        </ItemMenu>
+        <ItemMenu>
+          <ItemName>Sign Out</ItemName>
+          <Feather name="log-out" color="#666C8E" size={20} />
+        </ItemMenu>
+      </Menu>
+
     </Container>
   )
 };
--- src/pages/Profile/styles.ts
+++ src/pages/Profile/styles.ts
@@ -5,3 +5,52 @@ export const Container = styled.View`
   background-color: #fff;
   padding: 70px 20px 20px 20px;
 `;
+
+export const Info = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const ImgUser = styled.Image`
+  width: 72px;
+  height: 72px;
+  border-radius: 36px;
+`;
+
+export const InfoData = styled.View`
+  flex-direction: column;
+  margin-left: 24px;
+`;
+
+export const Name = styled.Text`
+  font-family: 'OpenSans_600SemiBold';
+  font-size: 16px;
+  color: #333647;
+`;
+
+export const Email = styled.Text`
+  font-family: 'OpenSans_400Regular';
+  font-size: 14px;
+  color: #7C82A1;
+`;
+
+export const Menu = styled.View`
+  margin-top: 32px;
+`;
+
+export const ItemMenu = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  background: #F3F4F6;
+  border-radius: 12px;
+  padding: 16px 24px;
+  margin-bottom: 16px;
+  height: 56px;
+`;
+
+export const ItemName = styled.Text`
+  font-family: 'OpenSans_600SemiBold';
+  font-size: 16px;
+  color: #666C8E;
+`;
